65b1eff6ee5d375346c907b01c90ff75dedfbe5e
Charlotte Natasha <natashaonyango97@gmail.com>
2022-05-01 16:11:28 +0200

    added classes, cofig.py and request.py

---
+++ app/config.py
@@ -0,0 +1,6 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything/{}?api_key={}'
+    
--- app/views.py
+++ app/views.py
@@ -1,8 +1,30 @@
 from flask import render_template
 from app import app
+from newsapi import NewsApiClient
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-   
+# @app.route('/')
+# def index():
+#     newsapi = NewsApiClient(api_key="4c212547c2de4b73a5917d14dfba0343")   
+#     topheadlines = newsapi.get_top_headlines(sources = "Fox Business")
+
+#     articles = topheadlines['articles']
+
+#     desc = []
+#     news = []
+#     img = []
+
+#     for i in range (len(articles)):
+#         myArticles = [articles[i]]
+
+
+#         news.append(myArticles['title'])
+#         desc.append(myArticles['description'])
+#         img.append(myArticles['urlToImage'])
+ 
+#     myList = zip(news, desc, img)
+
+#     return render_template('index.html', context= myList)
