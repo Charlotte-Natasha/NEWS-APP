68372dd6b543228ecfea7deaf0a62732c1b16194
Charlotte Natasha <natashaonyango97@gmail.com>
2022-05-01 21:52:58 +0200

    added bootstrap cdn and h1

--- app/views.py
+++ app/views.py
@@ -9,25 +9,3 @@ def index():
     '''
     return render_template("index.html")
 
-# @app.route('/')
-# def index():
-#     newsapi = NewsApiClient(api_key="4c212547c2de4b73a5917d14dfba0343")   
-#     topheadlines = newsapi.get_top_headlines(sources = "Fox Business")
-
-#     articles = topheadlines['articles']
-
-#     desc = []
-#     news = []
-#     img = []
-
-#     for i in range (len(articles)):
-#         myArticles = [articles[i]]
-
-
-#         news.append(myArticles['title'])
-#         desc.append(myArticles['description'])
-#         img.append(myArticles['urlToImage'])
- 
-#     myList = zip(news, desc, img)
-
-#     return render_template('index.html', context= myList)
