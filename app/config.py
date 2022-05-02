0ebe3dca0ec5172404237b98aa718b7e8a58c931
Charlotte Natasha <natashaonyango97@gmail.com>
2022-05-02 19:31:29 +0200

    added request and model folder in app folder

--- app/config.py
+++ app/config.py
@@ -4,3 +4,27 @@ class Config:
     '''
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey=API_KEY'
     
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
