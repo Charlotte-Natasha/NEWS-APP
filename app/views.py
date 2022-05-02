b5156106028c7917ee2e0ffecfd1d7d8e4c86585
Charlotte Natasha <natashaonyango97@gmail.com>
2022-05-02 18:41:49 +0200

    added news.html for the sources

--- app/views.py
+++ app/views.py
@@ -8,6 +8,6 @@ def index():
     View root page function that returns the index page and its data
     '''
     
-    title = 'FOXNEWS'
+    title = 'Current Affairs'
     return render_template("index.html", title = title)
 
