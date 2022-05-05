a1325e6e7a7d5297bd928cddb3c2d633bee30cc4
Charlotte Natasha <natashaonyango97@gmail.com>
2022-05-05 13:07:10 +0200

    styled index cards

--- app/models/news.py
+++ app/models/news.py
@@ -3,7 +3,8 @@ class News:
     Class to define news 
     '''
 
-    def __init__(self, author, title, description, urlToImage, content, publishedAt ):
+    def __init__(self, id, author, title, description, urlToImage, content, publishedAt ):
+        self.id = id
         self.author = author
         self.title = title
         self.description = description
